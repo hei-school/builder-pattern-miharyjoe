0a07576f6aa5809672b1ccbc9f8134d65b12911b
miharyjoel <miharyjoel@gmail.com>
2024-02-05 18:56:09 +0300

    chore: add in main an exemple of the car builder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Director director = new Director();
+    CarBuilder carBuilder = new CarBuilder();
+    CarManualBuilder manualBuilder = new CarManualBuilder();
+
+    // Construct a sports car
+    director.constructSportsCar(carBuilder);
+    Car car = carBuilder.getProduct();
+    System.out.println("--- Sports Car Details ---");
+    System.out.println("Engine: " + car.getEngine());
+    System.out.println("Has Trip Computer: " + car.getHasTripComputer());
+    System.out.println("Has GPS: " + car.getHasGPS());
+
+    // Print the sports car manual sections
+    director.constructSportsCar(manualBuilder);
+    Manual manual = manualBuilder.getProduct();
+    String[] manualSections = manual.getSections();
+    System.out.println("--- Sports Car Manual Sections ---");
+    for (String section : manualSections) {
+      System.out.println(section);
+    }
   }
 }
